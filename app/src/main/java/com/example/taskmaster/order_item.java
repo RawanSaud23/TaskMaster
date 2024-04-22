a1702a7f852f54053e33b9b5b68512c48d8e7b8d
RawanSaud23 <srawanalsaif@gmail.com>
2024-04-22 17:49:16 +0300

    ..

--- app/src/main/java/com/example/taskmaster/order_item.java
+++ app/src/main/java/com/example/taskmaster/order_item.java
@@ -1,7 +1,14 @@
 package com.example.taskmaster;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +17,34 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class order_item extends Activity {
+
+    private TextView text;
+    private Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_item);
+
+        text= findViewById(R.id.textClock);
+        btn= findViewById(R.id.editButton);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDilog();
+
+            }
+        });
+
+    }
+    private void openDilog(){
+        TimePickerDialog dialog= new TimePickerDialog(this, R.style.DialogTheme, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                text.setText(String.valueOf(hourOfDay)+":"+String.valueOf(minute));
+
+            }
+        }, 15, 50, true);
     }
 }
