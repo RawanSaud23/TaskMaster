3a180616e86c3dcc160c79a274fee389deb5d172
Ghalaalaskar <ghalaturki555@gmail.com>
2024-04-23 01:58:10 +0300

    h

--- app/src/main/java/com/example/taskmaster/PlumbingSub.java
+++ app/src/main/java/com/example/taskmaster/PlumbingSub.java
@@ -2,13 +2,23 @@ package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class PlumbingSub extends AppCompatActivity {
-
+RelativeLayout w ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plumbing_sub);
+
+        w=findViewById(R.id.washbasins);
+        w.setOnClickListener(new View.OnClickListener() { @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(getApplicationContext(), subcategorydetailswashbasins.class);
+            startActivity(intent);
+        } });
     }
 }
