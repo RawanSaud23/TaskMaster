c5b692c9e6eca6d68a5bf49d3c5e014e1faff839
reemalhawas1 <raalhawas@hotmail.com>
2024-04-22 23:07:25 +0300

    d

--- app/src/main/java/com/example/taskmaster/Login.java
+++ app/src/main/java/com/example/taskmaster/Login.java
@@ -2,13 +2,52 @@ package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
+    EditText username, password;
+    Button signin, signup;
+    DataBaseHelper DB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username = findViewById(R.id.editTextText2);
+        password = findViewById(R.id.editTextTextPassword);
+        signin = findViewById(R.id.button6);
+        signup = findViewById(R.id.button4);
+        DB = new DataBaseHelper(this);
+
+        signin.setOnClickListener(new View.OnClickListener() { @Override
+        public void onClick(View view) {
+            String user = username.getText().toString();
+            String pass = password.getText().toString();
+            if(user.equals("")||pass.equals(""))
+                Toast.makeText(Login.this, "Please fill all the required fields", Toast.LENGTH_SHORT).show();
+            else{
+                Boolean check = DB.checkUsernamePassword(user, pass);
+                if (check){
+                    Toast.makeText(Login.this, "successful login in ", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getApplicationContext(),categorieshomepage.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(Login.this, "Wrong username or password", Toast.LENGTH_SHORT).show(); }
+            } }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() { @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(getApplicationContext(), Signup.class);
+            startActivity(intent);
+        } });
+
+
     }
 }
