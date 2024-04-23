35939183b78ed5d58bbcdd5991f62848cde923e7
RawanSaud23 <srawanalsaif@gmail.com>
2024-04-23 19:11:50 +0300

    nav to edit

--- app/src/main/java/com/example/taskmaster/order_item.java
+++ app/src/main/java/com/example/taskmaster/order_item.java
@@ -1,18 +1,15 @@
 package com.example.taskmaster;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
