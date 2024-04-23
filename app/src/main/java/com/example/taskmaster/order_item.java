b6d55136b1c83fd7992b63daccd837ec942095bd
RawanSaud23 <srawanalsaif@gmail.com>
2024-04-23 18:10:26 +0300

    onclick

--- app/src/main/java/com/example/taskmaster/order_item.java
+++ app/src/main/java/com/example/taskmaster/order_item.java
@@ -1,14 +1,12 @@
 package com.example.taskmaster;
 
 import android.app.Activity;
-import android.app.DatePickerDialog;
-import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.TextView;
-import android.widget.TimePicker;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,26 +14,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class order_item extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
 
-    private TextView text;
-    private Button btn;
+public class order_item extends AppCompatActivity {
+
+    Button editButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_item);
 
-        text= findViewById(R.id.textClock);
-        btn= findViewById(R.id.editButton);
+        editButton = findViewById(R.id.editButton);
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        editButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                // Handle button click event here
+                // Redirect to another page
+                Intent intent = new Intent(order_item.this, udpateTimeLoc.class);
+                startActivity(intent);
             }
         });
-
     }
 
 }
